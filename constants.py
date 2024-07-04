7386b1f7d25115fb889a9130ae43fb0ead88dc5c
Иван Ермаков <132463482+ermaksmak@users.noreply.github.com>
2024-07-04 11:53:26 +0400

    Update constants.py

---
+++ constants.py
@@ -0,0 +1,5 @@
+MAIN_URL = ('https://stellarburgers.nomoreparties.site')
+
+LOGIN_PAGE = ('https://stellarburgers.nomoreparties.site/login')
+
+PROFILE_CABINET = ('https://stellarburgers.nomoreparties.site/account/profile')
